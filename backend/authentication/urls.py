2d903247e8c1660dcfae7a3083e47c150fc58dd2
JosueSinaca <sinaca56@gmail.com>
2025-05-31 15:13:23 -0600

    github login

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import MFASetupView, MFAVerifyView, MFADisableView
+from . import views
 
 urlpatterns = [
     path('mfa/setup/', MFASetupView.as_view(), name='mfa-setup'),
     path('mfa/verify/', MFAVerifyView.as_view(), name='mfa-verify'),
     path('mfa/disable/', MFADisableView.as_view(), name='mfa-disable'),
+    path('github/', views.github_login_redirect, name='github_login'),
+    path('github/callback/', views.github_callback, name='github_callback'),
+    path('check-status/', views.check_auth_status, name='check_auth_status'),
 ]
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -134,7 +134,7 @@ const Login = () => {
               </a>
 
               <a
-                href="#"
+                href='http://localhost:8000/api/auth/github/'
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
               >
                 <svg className="h-5 w-5 fill-[#24292F]" aria-hidden="true" viewBox="0 0 20 20">
